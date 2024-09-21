c281c0b2748340e78a555152a216c89059b16acd
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2024-09-21 12:16:03 +0200

    [INIT] added new http server

---
+++ _rel/backend_release/lib/backend-0.1.0/ebin/backend.app
@@ -0,0 +1,10 @@
+{application, 'backend', [
+	{description, "New project"},
+	{vsn, "0.1.0"},
+	{modules, ['backend_app','backend_sup','hello_handler']},
+	{registered, [backend_sup]},
+	{applications, [kernel,stdlib,cowboy]},
+	{optional_applications, []},
+	{mod, {backend_app, []}},
+	{env, []}
+]}.
---
+++ _rel/backend_release/lib/backend-0.1.0/src/backend_app.erl
@@ -0,0 +1,20 @@
+-module(backend_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", hello_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+	backend_sup:start_link().
+
+stop(_State) ->
+	ok.
